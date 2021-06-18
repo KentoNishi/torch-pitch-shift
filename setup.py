ad4ea0b29dd5eceab5491467a4c16b9ecf7d9b3b
Kento Nishi <kento24gs@outlook.com>
2021-06-17 17:50:30 -0700

    initial commit

---
+++ setup.py
@@ -0,0 +1,31 @@
+import setuptools
+import sys
+
+with open("readme.md", "r") as f:
+    long_description = f.read()
+
+
+with open("requirements.txt", "r") as f:
+    requirements = f.read().split("\n")
+
+versionName = sys.argv[1].replace("refs/tags/v", "")
+del sys.argv[1]
+
+setuptools.setup(
+    name="gpu_pitch_shift",
+    version=versionName,
+    author="KentoNishi",
+    author_email="kento24gs@outlook.com",
+    description=long_description.split("\n")[1],
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/KentoNishi/gpu_pitch_shift",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires=">=3.4",
+    install_requires=requirements,
+)
