c085c6666257583149bb85499562d1614ceb4ece
Kento Nishi <kento24gs@outlook.com>
2021-07-02 01:24:51 -0700

    add tests

--- example.py
+++ example.py
@@ -14,29 +14,37 @@ sample = torch.tensor(
     device="cuda" if torch.cuda.is_available() else "cpu",
 )
 
-# pitch up by 12 semitones
-up = pitch_shift(sample, 12, SAMPLE_RATE)
-wavfile.write(
-    "./wavs/shifted_octave_+1.wav",
-    SAMPLE_RATE,
-    np.swapaxes(up.cpu()[0].numpy(), 0, 1).astype(dtype),
-)
 
-# pitch down by 12 semitones
-down = pitch_shift(sample, -12, SAMPLE_RATE)
-wavfile.write(
-    "./wavs/shifted_octave_-1.wav",
-    SAMPLE_RATE,
-    np.swapaxes(down.cpu()[0].numpy(), 0, 1).astype(dtype),
-)
-
-# get shift ratios that are fast (between +1 and -1 octaves)
-for ratio in get_fast_shifts(SAMPLE_RATE):
-    print("Shifting", ratio)
+def pitch_shift_12_up():
+    # pitch up by 12 semitones
+    up = pitch_shift(sample, 12, SAMPLE_RATE)
+    assert up.shape == sample.shape
     wavfile.write(
-        f"./wavs/shifted_ratio_{ratio.numerator}-{ratio.denominator}.wav",
+        "./wavs/shifted_octave_+1.wav",
         SAMPLE_RATE,
-        np.swapaxes(
-            pitch_shift(sample, ratio, SAMPLE_RATE).cpu()[0].numpy(), 0, 1
-        ).astype(dtype),
+        np.swapaxes(up.cpu()[0].numpy(), 0, 1).astype(dtype),
     )
+
+
+def pitch_shift_12_down():
+    # pitch down by 12 semitones
+    down = pitch_shift(sample, -12, SAMPLE_RATE)
+    assert down.shape == sample.shape
+    wavfile.write(
+        "./wavs/shifted_octave_-1.wav",
+        SAMPLE_RATE,
+        np.swapaxes(down.cpu()[0].numpy(), 0, 1).astype(dtype),
+    )
+
+
+def pitch_shift_to_fast_ratios():
+    # get shift ratios that are fast (between +1 and -1 octaves)
+    for ratio in get_fast_shifts(SAMPLE_RATE):
+        print("Shifting", ratio)
+        shifted = pitch_shift(sample, ratio, SAMPLE_RATE)
+        assert shifted.shape == sample.shape
+        wavfile.write(
+            f"./wavs/shifted_ratio_{ratio.numerator}-{ratio.denominator}.wav",
+            SAMPLE_RATE,
+            np.swapaxes(shifted.cpu()[0].numpy(), 0, 1).astype(dtype),
+        )
